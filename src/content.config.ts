2168be91942d13766a6e71135c31d3217fe43ec1
Rory McLeod <mcleodnw@gmail.com>
2025-02-05 21:48:59 -0800

    list of camps and newsletters

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from 'astro:content';
+import { glob, file } from 'astro/loaders';
+
+//const newsPosts = defineCollection({
+//    loader: glob({ pattern: "**/*.md", base: "./src/data/news" }),
+//    schema: z.object({
+//        date: z.date(),
+//        file: z.string(),
+//    })
+//});
+
+const newsletters = defineCollection({
+    loader: file("src/data/newsletters.yaml"),
+});
+
+const camps = defineCollection({
+    loader: file("src/data/camps.yaml"),
+});
+
+export const collections = { newsletters, camps };
