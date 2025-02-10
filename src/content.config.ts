e98dafe719a27140fe5fefaa5cd0f4cb5dcdab9b
Rory McLeod <mcleodnw@gmail.com>
2025-02-09 16:26:03 -0800

    sort camps by date

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,24 +1,20 @@
 import { defineCollection, z } from 'astro:content';
-import { glob, file } from 'astro/loaders';
+import { file } from 'astro/loaders';
 
 const newsletters = defineCollection({
     loader: file("src/data/newsletters.yaml"),
 });
 
-const camps = defineCollection({
-    loader: file("src/data/camps.yaml"),
-});
-
 const caeWorkshops = defineCollection({
     loader: file("src/data/cae/workshops.yaml"),
-})
+});
 
 const campJobGroups = defineCollection({
     loader: file("src/data/camp-job-groups.yaml"),
-})
+});
 
 const campJobs = defineCollection({
     loader: file("src/data/campJobs.yaml"),
-})
+});
 
-export const collections = { newsletters, camps, caeWorkshops, campJobGroups, campJobs };
+export const collections = { newsletters, caeWorkshops, campJobGroups, campJobs };
