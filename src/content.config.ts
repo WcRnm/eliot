404e8ec3edbabe3a79d793cccf5939b5706360f2
Rory McLeod <mcleodnw@gmail.com>
2025-02-10 19:28:00 -0800

    CAE pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,10 +5,6 @@ const newsletters = defineCollection({
     loader: file("src/data/newsletters.yaml"),
 });
 
-const caeWorkshops = defineCollection({
-    loader: file("src/data/cae/workshops.yaml"),
-});
-
 const campJobGroups = defineCollection({
     loader: file("src/data/camp-job-groups.yaml"),
 });
@@ -17,9 +13,12 @@ const campJobs = defineCollection({
     loader: file("src/data/campJobs.yaml"),
 });
 
-
 const camps = defineCollection({
-    loader: glob({ pattern: '**/*.mdx', base: './src/pages/camps'})
+    loader: glob({ pattern: '**/*.mdx', base: './src/pages/camps'}),
 });
 
-export const collections = { newsletters, caeWorkshops, campJobGroups, campJobs, camps };
+const workshops = defineCollection({
+    loader: glob({ pattern: '**/*.mdx', base: './src/pages/cae'}),
+});
+
+export const collections = { newsletters, workshops, campJobGroups, campJobs, camps };
